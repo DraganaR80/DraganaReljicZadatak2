f3867deefe20ed9adc2c9e08b4b829c3cab752a8
Dragana Reljic <reljicdragana@yahoo.com>
2022-12-10 15:43:04 +0100

    Pocetak drugog zadatka

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="images/favicon.png">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Prodavnica vunice </title>
+</head>
+<body>
+
+<!-- <div class="header-div">
+        <ul class="menu">
+        
+            <li class="menu-items">  <a href="index.php"> Početna </a></li>
+            <li class= "menu-items">  <a href="Proizvodi.html">Proizvodi</a></li>
+            <li class= "menu-items" > <a href="Porudžbenica.html"> Poručite</a></li>
+            
+        </ul> -->
+      
+        </div>
+    <Div class="Forma">
+    <div class="centralni">
+    <form method action="Post">
+<h1 class="pozdrav">Dobro došli </h1>
+<br> <br>
+<div class="slika">
+<img src="images/slika forma.png" width="300px" height="300px">
+
+</div>
+
+<br> <br> <br>
+
+</form>
+
+            <input type="text" name="username" placeholder="Korisničko ime" id="username">
+       <br><br>
+    
+         <input type="password" name="password" placeholder="Lozinka" id="password">
+      <br><br>
+        <button class="pošalji">Pošalji</button>
+    </form>
+    </div>
+    </div>
+
+
+
+
+
+
+
+    </Div>
+</body>
+</html>
